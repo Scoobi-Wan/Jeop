63fda5c771024d15ad1d795b38cfa6acdd71083c
Paul Rich <paulrichdev@gmail.com>
2021-05-05 14:42:24 -0700

    moved scraping function into Controller class

--- Main.java
+++ Main.java
@@ -10,52 +10,17 @@ import org.jsoup.select.Elements;
 
 public class Main {
 
+
+
+
     public static void main(String[] args) {
 
-        try {
-            Document doc = Jsoup.connect("https://www.j-archive.com/showgame.php?game_id=6695").get();
-
-            System.out.printf("Title: %s\n", doc.title());
-
-            Elements categories = doc.getElementsByClass("category_name");
-            Elements clues = doc.getElementsByClass("clue_text");
-            Elements clueDivs = doc.getElementsByClass("clue");
-
-            for (Element category: categories) {
-                System.out.println(category.text());
-            }
-
-            for (Element clue: clues) {
-                System.out.println(clue.text());
-            }
-
-            for (Element clueDiv: clueDivs) {
-                String[] clueDivPieces = String.valueOf(clueDiv).split("correct_response&quot;>");
-                if (clueDivPieces.length > 1) {
-                    String correctResponse = clueDivPieces[1].replace("&lt;i&gt;", "");
-                    correctResponse = correctResponse.replace("&lt;//i&gt;", "");
-                    clueDivPieces = correctResponse.split("&lt;");
-                    correctResponse = clueDivPieces[0].replace("&amp;", "&");
-                    correctResponse = correctResponse.replace("&quot;", "\"");
-                    correctResponse = correctResponse.replace("<i>", "");
-                    correctResponse = correctResponse.replace("</i>", "");
-                    clueDivPieces = correctResponse.split("</em>");
-                    correctResponse = clueDivPieces[0];
-
-                    System.out.println(correctResponse);
-                }
-
-            }
-
-
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        Controller controller = new Controller();
+        controller.scrapeGames();
 
     }
+
+
 
 
 }
